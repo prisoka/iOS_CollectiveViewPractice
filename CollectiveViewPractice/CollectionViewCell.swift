86f712a56edb21a7d3ffdcb0227b4dda7756161b
Priscilla Okawa <prisoka@gmail.com>
2018-09-05 15:22:11 -0700

    playing with collective view

---
+++ CollectiveViewPractice/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  CollectiveViewPractice
+//
+//  Created by Priscilla Okawa on 5/9/18.
+//  Copyright © 2018 Priscilla Okawa. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dogPhoto: UIImageView!
+    @IBOutlet weak var dogName: UILabel!
+    @IBOutlet weak var age: UILabel!
+    @IBOutlet weak var size: UILabel!
+}
--- CollectiveViewPractice/ViewController.swift
+++ CollectiveViewPractice/ViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    let dogName = ["Aquila", "Flora", "Happy", "Peanut Butter"]
+    
+    let dogPhoto = [UIImage(named: "Aquila"), UIImage(named: "Flora"), UIImage(named: "Happy"), UIImage(named: "PeanutButter")]
+    
+    let ages = ["6", "5", "4", "2"]
+    
+    let sizes = ["large", "medium", "medium", "11:11 AM"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("requests view controller did load")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dogName.count
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: CollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        
+        cell.dogPhoto.image = dogPhoto[indexPath.row]
+        cell.dogName.text = dogName[indexPath.row]
+        cell.age.text = ages[indexPath.row]
+        cell.size.text = sizes[indexPath.row]
+        
+        return cell
+    }
 }
+
 
